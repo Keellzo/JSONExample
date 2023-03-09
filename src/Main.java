aafaf29109cd2ff005fb07bf4d8c32a1202f6b6b
Kelly <kellygashi00@gmail.com>
2023-03-09 11:17:40 +0100

    Skapat ett json object och skrivit och hämtat data, namn och ålder.

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,6 @@ public class Main {
         System.out.println("Mitt namn är " + jsonOb.get("name"));
         System.out.println("Jag är " + jsonOb.get("age") + " år gammal.");
 
+        System.out.println(jsonOb);
     }
 }
